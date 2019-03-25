561158eb6a71994d1eb1b312d9e954040e454bae
sh7411usa <48100468+sh7411usa@users.noreply.github.com>
2019-03-25 13:01:00 +0200

    Add files via upload
    The Fast Delete Script - in it's early stage. (Functional)

---
+++ fastDel.ps1
@@ -0,0 +1,5 @@
+﻿$empty_dir = "empty_dir"
+$dir_to_wipe = read-host "Enter Full Path of Directory to Delete, or Drop the Folder Here, and Press [ENTER]"
+mkdir $empty_dir
+robocopy $empty_dir $dir_to_wipe /mir /r:0 /w:0 /e
+rmdir $empty_dir $dir_to_wipe
